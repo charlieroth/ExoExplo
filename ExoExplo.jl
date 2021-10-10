0da293bfce74e9e6b35d7df6a19603156c46e7c6
Charles Roth <charlieroth4@icloud.com>
2021-10-10 18:18:41 +0200

    init

---
+++ ExoExplo.jl
@@ -0,0 +1,30 @@
+### A Pluto.jl notebook ###
+# v0.16.1
+
+using Markdown
+using InteractiveUtils
+
+# ╔═╡ 021b4546-29e0-11ec-1c2b-09b93d8cdd9a
+md"
+# Exoplanet Data Exploration
+
+A notebook for exploring the NASA Exoplanet Archive
+
+### What is an Exoplanet?
+[Wikipedia](https://en.wikipedia.org/wiki/Exoplanet)
+
+An planet outside the Solar System
+
+### Points of Exploration
+1. Discovery by year
+2. Discovery by method
+3. Discovery by facilities
+4. Relationship beetween discovery {year, method, facilities} and number of stars
+5. Relationship beetween discovery {year, method, facilities} and number of planets
+6. Relationship beetween discovery {year, method, facilities} and orbital period
+7. Relationship beetween discovery {year, method, facilities} and planet radius
+8. Relationship beetween discovery {year, method, facilities} and planet mass
+"
+
+# ╔═╡ Cell order:
+# ╟─021b4546-29e0-11ec-1c2b-09b93d8cdd9a
---
+++ README.md
@@ -0,0 +1,5 @@
+# Exoplanet Data Exploration
+
+Pluto notebook to learn about Exoplanets, what we know about them and how they are discovered
+
+`NASA_Exoplanet_Data_Set.csv.zip` downloaded on October 10th, 2021
